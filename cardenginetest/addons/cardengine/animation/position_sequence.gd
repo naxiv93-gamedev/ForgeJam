b45828f6465d4dbea00c9e183ae459cb83cd58c7
ivandido <ivandiaz93@gmail.com>
2023-09-23 17:06:30 +0200

    Initial Commit

---
+++ cardenginetest/addons/cardengine/animation/position_sequence.gd
@@ -0,0 +1,8 @@
+@tool
+class_name PositionSequence
+extends AnimationSequence
+
+
+func _init(from_mode: int, to_mode: int) -> void:
+	super(from_mode, to_mode)
+	pass
---
+++ cardenginetest/addons/cardengine/animation/rotation_sequence.gd
@@ -0,0 +1,9 @@
+@tool
+class_name RotationSequence
+extends AnimationSequence
+
+
+func _init(from_mode: int, to_mode: int) -> void:
+	super(from_mode, to_mode)
+	pass
+
